21d63a2e108d489a22ccd0657a06c48b98a911d4
yonsse <3318yonsse@naver.com>
2020-12-13 21:38:33 +0900

    1212 modi

--- BMI/app/src/main/java/com/androidlec/bmi/MainActivity.java
+++ BMI/app/src/main/java/com/androidlec/bmi/MainActivity.java
@@ -23,13 +23,14 @@ public class MainActivity extends Activity {
 
         Button start = findViewById(R.id.start);
         Button btn = findViewById(R.id.btnCalc);
+        Button goback = findViewById(R.id.goback);
 
         EditText height = findViewById(R.id.height);
         EditText weight = findViewById(R.id.weight);
         TextView result = findViewById(R.id.result);
         ImageView resulImage = findViewById(R.id.image);
-        ImageView good = findViewById(R.id.good);
 
+        LinearLayout text = findViewById(R.id.text);
         LinearLayout info = findViewById(R.id.infoImage);
         LinearLayout insert = findViewById(R.id.insert);
 
@@ -38,20 +39,26 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
 
-                if(info.getVisibility() == v.VISIBLE){
+                if(info.getVisibility() == v.VISIBLE) {
                     info.setVisibility(v.INVISIBLE);
                     insert.setVisibility(v.VISIBLE);
-                    start.setVisibility(v.INVISIBLE);
 
-                } else if(insert.getVisibility() == v.VISIBLE){
-                    insert.setVisibility(v.INVISIBLE);
-                    info.setVisibility(v.VISIBLE);
+
                 }
             }
         });
 
 
+        goback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(insert.getVisibility() == v.VISIBLE) {
+                    insert.setVisibility(v.INVISIBLE);
+                    info.setVisibility(v.VISIBLE);
 
+                }
+            }
+        });
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,48 +68,46 @@ public class MainActivity extends Activity {
                 double heightValue = Double.parseDouble(height.getText().toString());
                 double weightValue = Double.parseDouble(weight.getText().toString());
 
-                double heightCm = heightValue * 0.01;
-
-                double BMI = weightValue / (heightCm*heightCm);
-
-                String BMIresult = String.format("%.2f", BMI);
 
 
 
 
+                if (height.getText().length()==0 || weight.getText().length()==0 ){
+                    String str = "값을 입력해주세요";
+                    Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
 
+                } else{
+                    double heightCm = heightValue * 0.01;
 
+                    double BMI = weightValue / (heightCm*heightCm);
 
-                if (height.length() == 0) {
-//
-                    Toast.makeText(getApplicationContext(), "값을 입력해주세요", Toast.LENGTH_LONG).show();
+                    String BMIresult = String.format("%.2f", BMI);
 
-                } else if (weight.length() == 0){
+                    if(BMI <= 18.5) {
 
-                    Toast.makeText(getApplicationContext(), "값을 입력해주세요", Toast.LENGTH_LONG).show();
+                        resulImage.setImageResource(R.drawable.first);
+                        result.setText("당신의 BMI지수는 "+BMIresult+" 입니다.");
+                    } else if (BMI >= 18.5 && BMI <=22.9){
+                        resulImage.setImageResource(R.drawable.second);
+                        result.setText("당신의 BMI지수는 "+BMIresult+" 입니다.");
+                    } else if (BMI >= 23 && BMI <= 24.9){
+                        resulImage.setImageResource(R.drawable.thrid);
+                        result.setText("당신의 BMI지수는 "+BMIresult+" 입니다.");
+                    } else if (BMI >= 25 && BMI <=29.9){
+                        resulImage.setImageResource(R.drawable.fourth);
+                        result.setText("당신의 BMI지수는 "+BMIresult+" 입니다.");
+                    }else {
+                        resulImage.setImageResource(R.drawable.fifth);
+                        result.setText("당신의 BMI지수는 "+BMIresult+" 입니다.");
+                    }
                 }
 //
-                if(BMI <= 18.5) {
-                    resulImage.setImageResource(R.drawable.first);
-
-                } else if (BMI >= 18.5 && BMI <=22.9){
-                    resulImage.setImageResource(R.drawable.second);
-
-                } else if (BMI >= 23 && BMI <= 24.9){
-                    resulImage.setImageResource(R.drawable.thrid);
-                    good.setImageResource(R.drawable.dl01);
-                } else if (BMI >= 25 && BMI <=29.9){
-                    resulImage.setImageResource(R.drawable.fourth);
-
-                }else {
-                    resulImage.setImageResource(R.drawable.fifth);
-
-                }
 
 
 
 
-                result.setText(BMIresult);
+
+
             }
 
 
